cdbf82370b722c18341d42acc9cf40c16612e49e
hodanov <1031hoda@gmail.com>
2019-10-28 08:59:56 +0900

    Initialize a new http.Server struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,7 +22,14 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	infoLog.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	//err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
