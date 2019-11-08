853625cb19712965c99129766e9dbf05b084962b
hodanov <1031hoda@gmail.com>
2019-11-08 18:05:18 +0900

    Getting multiple records by sql queries

--- code/snippetbox/cmd/web/handlers.go
+++ code/snippetbox/cmd/web/handlers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+	"github.com/hodanov/snippetbox/pkg/models"
+	//"html/template"
 	"net/http"
 	"strconv"
 )
@@ -13,22 +15,32 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.serverError(w, err)
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
+
+	//	files := []string{
+	//		"./ui/html/home.page.tmpl",
+	//		"./ui/html/base.layout.tmpl",
+	//		"./ui/html/footer.partial.tmpl",
+	//	}
+	//
+	//	ts, err := template.ParseFiles(files...)
+	//	if err != nil {
+	//		app.serverError(w, err)
+	//		return
+	//	}
+	//
+	//	err = ts.Execute(w, nil)
+	//	if err != nil {
+	//		app.serverError(w, err)
+	//	}
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -37,7 +49,18 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%v", s)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
