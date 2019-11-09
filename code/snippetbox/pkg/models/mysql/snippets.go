d28cc6e8f40f3a27ce2e8c3188b70aab69174034
hodanov <1031hoda@gmail.com>
2019-11-09 23:28:52 +0900

    Add transaction

--- code/snippetbox/pkg/models/mysql/snippets.go
+++ code/snippetbox/pkg/models/mysql/snippets.go
@@ -14,18 +14,24 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 	stmt := `INSERT INTO snippets (title, content, created, expires)
 	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
-	result, err := m.DB.Exec(stmt, title, content, expires)
+	tx, err := m.DB.Begin()
+	defer tx.Rollback()
 	if err != nil {
-		return 0, nil
+		return 0, err
+	}
+
+	result, err := tx.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
 	}
 
 	id, err := result.LastInsertId()
 	if err != nil {
-		return 0, nil
+		return 0, err
 	}
 
+	err = tx.Commit()
 	return int(id), nil
-
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
@@ -48,32 +54,5 @@ func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 }
 
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	stmt := `SELECT id, title, content, created, expires FROM snippets
-	WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
-
-	rows, err := m.DB.Query(stmt)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	snippets := []*models.Snippet{}
-
-	for rows.Next() {
-		s := &models.Snippet{}
-
-		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
-		if err != nil {
-			return nil, err
-		}
-
-		snippets = append(snippets, s)
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return snippets, nil
+	return nil, nil
 }
