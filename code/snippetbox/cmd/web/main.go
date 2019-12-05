57ad89410243a519e8d3da65cca2ace4524d99a1
hodanov <1031hoda@gmail.com>
2019-12-05 09:35:59 +0900

    Modify ListenAndServe method to ListenAndServeTLS method

--- code/snippetbox/cmd/web/main.go
+++ code/snippetbox/cmd/web/main.go
@@ -72,6 +72,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
