86442a8e90e6d6d2bba7c4c3b79a64c3a0836530
hodanov <1031hoda@gmail.com>
2019-12-06 09:38:29 +0900

    Add a validation to signup form

--- code/snippetbox/pkg/forms/form.go
+++ code/snippetbox/pkg/forms/form.go
@@ -3,9 +3,15 @@ package forms
 import (
 	"fmt"
 	"net/url"
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
+
+// Use the regexp.MustCompile() function to parse a pattern and compile a regular expression for sanity checking the format of an email address.
+// This returns as *regexp.Regexp object, or panics in the event of an error.
+// Doing this once at runtime, and storing the compiled regular expression object in a variable, is more performant than re-compiling the pattern with every request.
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 // Create a custom Form struct, which anonymously embeds a url.Values object (to hold the form data) and an Errors field to hold any validation errors for the form data.
 type Form struct {
@@ -58,6 +64,30 @@ func (f *Form) PermittedValues(field string, opts ...string) {
 		}
 	}
 	f.Errors.Add(field, "This field is invalid")
+}
+
+// A MinLength method to check that a specific field in the form contains a minimum number of characters.
+// If the check fails then add the appropriate message to the form errors.
+func (f *Form) MinLength(field string, d int) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if utf8.RuneCountInString(value) < d {
+		f.Errors.Add(field, fmt.Sprintf("This field is too short (minimum is %d characters)", d))
+	}
+}
+
+// A MatchesPattern method to check that a specific field in the form matches a regular expression.
+// If the check fails then add the appropriate message to the form errors.
+func (f *Form) MatchesPattern(field string, pattern *regexp.Regexp) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if !pattern.MatchString(value) {
+		f.Errors.Add(field, "This field is invalid")
+	}
 }
 
 // A Valid method which returns true if there are no errors.
