1d0d633c07f2da13b8856fafe868181f50e47f04
hodanov <1031hoda@gmail.com>
2019-12-27 22:04:24 +0900

    Add end-to-end test

--- code/snippetbox/cmd/web/testutils_test.go
+++ code/snippetbox/cmd/web/testutils_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 )
@@ -26,6 +27,22 @@ type testServer struct {
 // of our custom testServer type.
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+
+	// Initialize a new cookie jar.
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Add the cookie jar to the client, so that response cookies are stored
+	// and then sent with subsequent requests.
+	ts.Client().Jar = jar
+
+	// Disable redirect-following for the client.
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
 	return &testServer{ts}
 }
 
