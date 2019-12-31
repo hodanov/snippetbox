e6aa3e8911c0d195da35ae6efab32e9f9014e48f
hodanov <1031hoda@gmail.com>
2019-12-31 16:43:50 +0900

    Add test user and modify dockerfile, add TestUserModelGet

--- db/initdb.d/01_create_table.sql
+++ db/initdb.d/01_create_table.sql
@@ -21,7 +21,8 @@ ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
 
 -- Test DB
 CREATE DATABASE test_snippetbox CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT CREATE, DROP, ALTER, INDEX, SELECT, INSERT, UPDATE, DELETE ON test_snippetbox.* TO 'test_web';
 
-CREATE USER 'test_web'@'localhost';
-GRANT CREATE, DROP, ALTER, INDEX, SELECT, INSERT, UPDATE, DELETE ON test_snippetbox.* TO 'test_web'@'localhost';
-ALTER USER 'test_web'@'localhost' IDENTIFIED BY 'pass';
+-- CREATE USER 'test_web'@'localhost';
+-- GRANT CREATE, DROP, ALTER, INDEX, SELECT, INSERT, UPDATE, DELETE ON test_snippetbox.* TO 'test_web'@'localhost';
+-- ALTER USER 'test_web'@'localhost' IDENTIFIED BY 'pass';
